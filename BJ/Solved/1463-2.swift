a5648441651d376ce8e627cbde4f1e1abe389d14
songmoro <wlqgkr@naver.com>
2025-03-11 15:45:31 +0900

    solve 1463

---
+++ BJ/Solved/1463-2.swift
@@ -0,0 +1,16 @@
+//2
+
+let N = Int(readLine()!)!
+var dp = [Int](repeating: Int.max, count: 1000001)
+(dp[0], dp[1]) = (-1, 0)
+
+if N == 1 { print(0) }
+else {
+    for i in 2...N {
+        if i % 3 == 0 { dp[i] = min(dp[i], dp[i / 3] + 1) }
+        if i % 2 == 0 { dp[i] = min(dp[i], dp[i / 2] + 1) }
+        dp[i] = min(dp[i], dp[i - 1] + 1)
+    }
+    
+    print(dp[N])
+}
