40a1b632968378e5dd26438fe5cd4e633f21f295
songmoro <wlqgkr@naver.com>
2025-03-14 18:17:23 +0900

    solve 2293

---
+++ BJ/Solved/2293-2.swift
@@ -0,0 +1,34 @@
+//
+//  2293-2.swift
+//  BJ
+//
+//  Created by 송재훈 on 3/14/25.
+//
+
+
+//3 10
+//1
+//2
+//5
+
+let nk = readLine()!.split(separator: " ").map { Int(String($0))! }
+let (n, k) = (nk[0], nk[1])
+
+var coins = [Int]()
+var dp = [Int](repeating: 0, count: k + 1)
+dp[0] = 1
+
+for _ in 0..<n {
+    let coin = Int(readLine()!)!
+    if coin <= k { coins.append(coin) }
+}
+
+for coin in coins {
+    for i in coin...k {
+        if !dp[i].addingReportingOverflow(dp[i - coin]).overflow {
+            dp[i] += dp[i - coin]
+        }
+    }
+}
+
+print(dp[k])
