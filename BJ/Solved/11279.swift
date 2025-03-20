37e42c57677f75661919a2b392d471c66f1d8b08
songmoro <wlqgkr@naver.com>
2025-03-20 18:59:48 +0900

    solve 11279

---
+++ BJ/Solved/11279.swift
@@ -0,0 +1,128 @@
+//
+//  11279.swift
+//  BJ
+//
+//  Created by 송재훈 on 3/20/25.
+//
+
+//13
+//0
+//1
+//2
+//0
+//0
+//3
+//2
+//1
+//0
+//0
+//0
+//0
+//0
+
+import Foundation
+
+struct PQ<T> {
+    var heap = [T]()
+    let compare: (T, T) -> Bool
+    
+    init(_ sort: @escaping (T, T) -> Bool) { self.compare = sort }
+    
+    var isEmpty: Bool { heap.isEmpty }
+    var count: Int { heap.count }
+    var top: T? { heap.first }
+    
+    mutating func push(_ value: T) {
+        heap.append(value)
+        
+        var i = heap.count - 1
+        
+        while i > 0, compare(heap[i], heap[(i - 1) / 2]) {
+            heap.swapAt(i, (i - 1) / 2)
+            i = (i - 1) / 2
+        }
+    }
+    
+    mutating func pop() -> T? {
+        guard !heap.isEmpty else { return nil }
+        if heap.count == 1 { return heap.removeFirst() }
+        heap.swapAt(0, heap.count - 1)
+        
+        var i = 0
+        let popped = heap.removeLast()
+        
+        while true {
+            let (left, right) = (i * 2 + 1, i * 2 + 2)
+            var next = i
+            
+            if left < heap.count, compare(heap[left], heap[next]) { next = left }
+            if right < heap.count, compare(heap[right], heap[next]) { next = right }
+            if i == next { break }
+            heap.swapAt(i, next)
+            i = next
+        }
+        
+        return popped
+    }
+}
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!) + [UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10 || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now - 48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index - 1
+        while now != 10, now != 32, now != 0 { now = read() }
+        
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+        
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index - 1
+        while now != 10, now != 32, now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let file = FileIO()
+
+let N = file.readInt()
+var pq = PQ<Int> { $0 > $1 }
+
+for _ in 0..<N {
+    let x = file.readInt()
+    
+    if x == 0 { print(pq.pop() ?? 0) }
+    else { pq.push(x) }
+}
